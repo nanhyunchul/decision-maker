623a841e685257d1ef88e610c3189f4ba831b013
Hyun <lhc4128@gmail.com>
2016-03-30 10:02:00 -0700

    I used image-responsive class to fit the picture into a grid and added lines of code to delete previous pictures when another pair of pictures is loaded. #2

--- public/default.js
+++ public/default.js
@@ -1,5 +1,4 @@
-var firstPicture = document.getElementById('picture-one');
-var secondPicture = document.getElementById('picture-two');
+var imagePanel = document.getElementById('image-panel');
 
 var pictureButton = document.getElementById('show-pictures');
 
@@ -10,15 +9,29 @@ pictureButton.addEventListener('click', function() {
 
   xhr.addEventListener('load', function() {
     var result = JSON.parse(xhr.responseText);
+    while (imagePanel.firstChild) {
+      imagePanel.removeChild(imagePanel.firstChild);
+    }
     if (xhr.status === 200) {
       var pictureOne = document.createElement('img');
       pictureOne.setAttribute('src', result[0]);
+      pictureOne.setAttribute('class', 'img-responsive')
 
       var pictureTwo = document.createElement('img');
       pictureTwo.setAttribute('src', result[1]);
+      pictureTwo.setAttribute('class', 'img-responsive')
 
-      firstPicture.appendChild(pictureOne);
-      secondPicture.appendChild(pictureTwo);
+      var firstPanel = document.createElement('div');
+      firstPanel.setAttribute('class', 'col-md-6');
+
+      var secondPanel = document.createElement('div');
+      secondPanel.setAttribute('class', 'col-md-6');
+
+      firstPanel.appendChild(pictureOne);
+      secondPanel.appendChild(pictureTwo);
+
+      imagePanel.appendChild(firstPanel);
+      imagePanel.appendChild(secondPanel);
     } else {
       alert('there was an error');
     }
