066eb1492e71238add69657eb01188ebe7560693
Hyun <lhc4128@gmail.com>
2016-03-30 13:41:07 -0700

    I separated XML request directories based on each button clicked and also added a counter on each picture object to see how many times that picture was chosen over the other one. #3

--- app.js
+++ app.js
@@ -3,64 +3,112 @@ var userData = [
     name: 'testUser',
     password: 'password',
     uploads: [
-      ['http://g-ecx.images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg', 'http://i3.mirror.co.uk/incoming/article6745694.ece/ALTERNATES/s615b/Ginger-tabby-cat.jpg'],
-      ['http://www.medhatspca.ca/sites/default/files/news_photos/2014-Apr-15/node-147/cute-little-cat.jpg', 'http://hdimagesnew.com/wp-content/uploads/2015/11/Cat-Cute-Kitten-HD-Wallpaper.jpg']
+      {
+        picture: ['http://g-ecx.images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg', 'http://i3.mirror.co.uk/incoming/article6745694.ece/ALTERNATES/s615b/Ginger-tabby-cat.jpg'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['http://www.medhatspca.ca/sites/default/files/news_photos/2014-Apr-15/node-147/cute-little-cat.jpg', 'http://hdimagesnew.com/wp-content/uploads/2015/11/Cat-Cute-Kitten-HD-Wallpaper.jpg'],
+        counter: [0, 0]
+      }
     ]
   },
   {
     name: 'testUser2',
     password: 'password',
     uploads: [
-      ['https://pajamasmed.hs.llnwd.net/e1/user-content/31/files/2014/02/coke.jpg', 'https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Pepsi_logo.svg/1280px-Pepsi_logo.svg.png'],
-      ['http://defenders.org/sites/default/files/styles/large/public/tiger-dirk-freder-isp.jpg', 'http://dreamatico.com/data_images/lion/lion-8.jpg']
+      {
+        picture: ['https://pajamasmed.hs.llnwd.net/e1/user-content/31/files/2014/02/coke.jpg', 'https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Pepsi_logo.svg/1280px-Pepsi_logo.svg.png'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['http://defenders.org/sites/default/files/styles/large/public/tiger-dirk-freder-isp.jpg', 'http://dreamatico.com/data_images/lion/lion-8.jpg'],
+        counter: [0, 0]
+      }
     ]
   },
   {
     name: 'testUser3',
     password: 'password',
     uploads: [
-      ['https://www.drum.fit/wp-content/uploads/2016/03/cup-of-coffee.jpg', 'http://healthnotesonline.areavoices.com/files/2016/03/tea.jpg'],
-      ['http://2.bp.blogspot.com/-919AZkQmDnM/TjA98CLzW2I/AAAAAAAADLk/uOZgtfyasIg/s1600/yes.gif', 'http://www.filmnotes.net/wp-content/uploads/2015/12/No.jpg']
+      {
+        picture: ['https://www.drum.fit/wp-content/uploads/2016/03/cup-of-coffee.jpg', 'http://healthnotesonline.areavoices.com/files/2016/03/tea.jpg'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['http://2.bp.blogspot.com/-919AZkQmDnM/TjA98CLzW2I/AAAAAAAADLk/uOZgtfyasIg/s1600/yes.gif', 'http://www.filmnotes.net/wp-content/uploads/2015/12/No.jpg'],
+        counter: [0, 0]
+      }
     ]
   },
   {
     name: 'testUser4',
     password: 'password',
     uploads: [
-      ['http://www.sixthseal.com/archive/September2006/bacon_omelet_bacon_done.jpg', 'https://cdn.shopify.com/s/files/1/0206/9470/files/Bacon.jpg?69173'],
-      ['http://www.keybiscaynesoccerclub.com/imgs/Left%20arrow.jpg', 'http://www.designofsignage.com/application/symbol/hospital/image/600x600/arrow-right.jpg']
+      {
+        picture: ['http://www.sixthseal.com/archive/September2006/bacon_omelet_bacon_done.jpg', 'https://cdn.shopify.com/s/files/1/0206/9470/files/Bacon.jpg?69173'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['http://www.keybiscaynesoccerclub.com/imgs/Left%20arrow.jpg', 'http://www.designofsignage.com/application/symbol/hospital/image/600x600/arrow-right.jpg'],
+        counter: [0, 0]
+      }
     ]
   },
   {
     name: 'testUser5',
     password: 'password',
     uploads: [
-      ['https://pbs.twimg.com/profile_images/573427740709298176/kfy24vLw.jpeg', 'http://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/249/2016/02/04165158/photo.jpg'],
-      ['http://www.clipartbest.com/cliparts/aTe/8oL/aTe8oLAT4.png', 'http://www.clipartbest.com/cliparts/xcg/q9o/xcgq9oocA.png']
+      {
+        picture: ['https://pbs.twimg.com/profile_images/573427740709298176/kfy24vLw.jpeg', 'http://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/249/2016/02/04165158/photo.jpg'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['http://www.clipartbest.com/cliparts/aTe/8oL/aTe8oLAT4.png', 'http://www.clipartbest.com/cliparts/xcg/q9o/xcgq9oocA.png'],
+        counter: [0, 0]
+      }
     ]
   },
   {
     name: 'testUser6',
     password: 'password',
     uploads: [
-      ['http://smitedatamining.com/wp-content/uploads/2015/12/playstation_4-3169634.jpg', 'http://gearnuke.com/wp-content/uploads/2015/05/xbox-one.png'],
-      ['http://static.comicvine.com/uploads/original/13/138572/2824589-13260-gamesrocks-superman.jpg', 'http://static.comicvine.com/uploads/original/11126/111264223/4978219-4440144695-21085.jpg']
+      {
+        picture: ['http://smitedatamining.com/wp-content/uploads/2015/12/playstation_4-3169634.jpg', 'http://gearnuke.com/wp-content/uploads/2015/05/xbox-one.png'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['http://static.comicvine.com/uploads/original/13/138572/2824589-13260-gamesrocks-superman.jpg', 'http://static.comicvine.com/uploads/original/11126/111264223/4978219-4440144695-21085.jpg'],
+        counter: [0, 0]
+      }
     ]
   },
   {
     name: 'testUser7',
     password: 'password',
     uploads: [
-      ['http://storage.googleapis.com/ix_choosemuse/uploads/2016/02/android-logo.png', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/2000px-Apple_logo_black.svg.png'],
-      ['https://pbs.twimg.com/profile_images/573984336271122432/k8vEBoCW.jpeg', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/DC_Comics_logo.svg/1024px-DC_Comics_logo.svg.png']
+      {
+        picture: ['http://storage.googleapis.com/ix_choosemuse/uploads/2016/02/android-logo.png', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/2000px-Apple_logo_black.svg.png'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['https://pbs.twimg.com/profile_images/573984336271122432/k8vEBoCW.jpeg', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/DC_Comics_logo.svg/1024px-DC_Comics_logo.svg.png'],
+        counter: [0, 0]
+      }
     ]
   },
   {
     name: 'testUser8',
     password: 'password',
     uploads: [
-      ['http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/box/macbook-box-hw-spacegray-201504?wid=569&hei=405&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1453495464940', 'http://static6.businessinsider.com/image/50dddabdeab8ea2063000001/heres-a-ridiculously-good-deal-on-a-windows-8-laptop-from-vizio.jpg'],
-      ['http://ecx.images-amazon.com/images/I/519XnhLGWCL.jpg', 'http://www.zeroto60times.com/blog/wp-content/uploads/2013/02/lamborghini-cars-logo-emblem.jpg']
+      {
+        picture: ['http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/box/macbook-box-hw-spacegray-201504?wid=569&hei=405&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1453495464940', 'http://static6.businessinsider.com/image/50dddabdeab8ea2063000001/heres-a-ridiculously-good-deal-on-a-windows-8-laptop-from-vizio.jpg'],
+        counter: [0, 0]
+      },
+      {
+        picture: ['http://ecx.images-amazon.com/images/I/519XnhLGWCL.jpg', 'http://www.zeroto60times.com/blog/wp-content/uploads/2013/02/lamborghini-cars-logo-emblem.jpg'],
+        counter: [0, 0]
+      }
     ]
   }
 ];
@@ -75,9 +123,26 @@ app.use(cookieParser());
 
 app.use(express.static('./public'));
 
-app.get('/picture', function(req, res) {
+app.get('/pictureOne', function(req, res) {
   var randomUser = userData[Math.floor(Math.random() * userData.length)];
-  var randomPicture = randomUser.uploads[Math.floor(Math.random() * randomUser.uploads.length)]
+  var randomPicture = randomUser.uploads[Math.floor(Math.random() * randomUser.uploads.length)];
+  randomPicture.counter[0]++;
+  console.log(randomPicture.counter[0]);
+  res.send(randomPicture);
+});
+
+app.get('/pictureTwo', function(req, res) {
+  var randomUser = userData[Math.floor(Math.random() * userData.length)];
+  var randomPicture = randomUser.uploads[Math.floor(Math.random() * randomUser.uploads.length)];
+  randomPicture.counter[1]++;
+  console.log(randomPicture.counter[1]);
+  res.send(randomPicture);
+});
+
+app.get('/skip', function(req, res) {
+  var randomUser = userData[Math.floor(Math.random() * userData.length)];
+  var randomPicture = randomUser.uploads[Math.floor(Math.random() * randomUser.uploads.length)];
+  console.log(randomPicture.counter);
   res.send(randomPicture);
 });
 
--- public/default.js
+++ public/default.js
@@ -1,26 +1,135 @@
 var imagePanel = document.getElementById('image-panel');
 imagePanel.setAttribute('id', 'images');
 
-function showPictures() {
+function showPicturesOne() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/picture');
+  xhr.open('GET', '/pictureOne');
   xhr.send();
 
   xhr.addEventListener('load', function() {
     var result = JSON.parse(xhr.responseText);
+    var resultPicture = result.picture
     while (imagePanel.firstChild) {
       imagePanel.removeChild(imagePanel.firstChild);
     }
     if (xhr.status === 200) {
       var pictureOne = document.createElement('img');
-      pictureOne.setAttribute('src', result[0]);
+      pictureOne.setAttribute('src', resultPicture[0]);
       pictureOne.setAttribute('class', 'img-responsive')
-      pictureOne.setAttribute('directory', '/picture');
+      pictureOne.setAttribute('directory', '/pictureOne');
 
       var pictureTwo = document.createElement('img');
-      pictureTwo.setAttribute('src', result[1]);
+      pictureTwo.setAttribute('src', resultPicture[1]);
       pictureTwo.setAttribute('class', 'img-responsive')
-      pictureTwo.setAttribute('directory', '/picture')
+      pictureTwo.setAttribute('directory', '/pictureTwo')
+
+      var buttonOne = document.createElement('button');
+      buttonOne.setAttribute('type', 'button');
+
+      var buttonTwo = document.createElement('button');
+      buttonTwo.setAttribute('type', 'button');
+
+      var firstPanel = document.createElement('div');
+      firstPanel.setAttribute('class', 'col-md-5');
+
+      var secondPanel = document.createElement('div');
+      secondPanel.setAttribute('class', 'col-md-5 col-md-offset-2');
+
+      var or = document.createElement('div');
+      or.setAttribute('class', 'col-md-2 text-center');
+      or.setAttribute('id', 'or')
+      or.textContent = 'OR';
+
+      buttonOne.appendChild(pictureOne);
+      buttonTwo.appendChild(pictureTwo);
+
+      firstPanel.appendChild(buttonOne);
+      secondPanel.appendChild(buttonTwo);
+
+      imagePanel.appendChild(firstPanel);
+      imagePanel.appendChild(or);
+      imagePanel.appendChild(secondPanel);
+    } else {
+      alert('there was an error');
+    }
+  })
+}
+
+function showPicturesTwo() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/pictureTwo');
+  xhr.send();
+
+  xhr.addEventListener('load', function() {
+    var result = JSON.parse(xhr.responseText);
+    var resultPicture = result.picture
+    while (imagePanel.firstChild) {
+      imagePanel.removeChild(imagePanel.firstChild);
+    }
+    if (xhr.status === 200) {
+      var pictureOne = document.createElement('img');
+      pictureOne.setAttribute('src', resultPicture[0]);
+      pictureOne.setAttribute('class', 'img-responsive')
+      pictureOne.setAttribute('directory', '/pictureOne');
+
+      var pictureTwo = document.createElement('img');
+      pictureTwo.setAttribute('src', resultPicture[1]);
+      pictureTwo.setAttribute('class', 'img-responsive')
+      pictureTwo.setAttribute('directory', '/pictureTwo')
+
+      var buttonOne = document.createElement('button');
+      buttonOne.setAttribute('type', 'button');
+
+      var buttonTwo = document.createElement('button');
+      buttonTwo.setAttribute('type', 'button');
+
+      var firstPanel = document.createElement('div');
+      firstPanel.setAttribute('class', 'col-md-5');
+
+      var secondPanel = document.createElement('div');
+      secondPanel.setAttribute('class', 'col-md-5 col-md-offset-2');
+
+      var or = document.createElement('div');
+      or.setAttribute('class', 'col-md-2 text-center');
+      or.setAttribute('id', 'or')
+      or.textContent = 'OR';
+
+      buttonOne.appendChild(pictureOne);
+      buttonTwo.appendChild(pictureTwo);
+
+      firstPanel.appendChild(buttonOne);
+      secondPanel.appendChild(buttonTwo);
+
+      imagePanel.appendChild(firstPanel);
+      imagePanel.appendChild(or);
+      imagePanel.appendChild(secondPanel);
+    } else {
+      alert('there was an error');
+    }
+  })
+}
+
+function showPicturesSkip() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/skip');
+  xhr.send();
+
+  xhr.addEventListener('load', function() {
+    var result = JSON.parse(xhr.responseText);
+    var resultPicture = result.picture
+    while (imagePanel.firstChild) {
+      imagePanel.removeChild(imagePanel.firstChild);
+    }
+    if (xhr.status === 200) {
+      var pictureOne = document.createElement('img');
+      pictureOne.setAttribute('src', resultPicture[0]);
+      pictureOne.setAttribute('class', 'img-responsive')
+      pictureOne.setAttribute('directory', '/pictureOne');
+
+      var pictureTwo = document.createElement('img');
+      pictureTwo.setAttribute('src', resultPicture[1]);
+      pictureTwo.setAttribute('class', 'img-responsive')
+      pictureTwo.setAttribute('directory', '/pictureTwo')
 
       var buttonOne = document.createElement('button');
       buttonOne.setAttribute('type', 'button');
@@ -58,8 +167,13 @@ var body = document.body;
 body.addEventListener('click', function(event) {
   event.preventDefault();
   var id = event.target.getAttribute('directory');
-  if (id == '/picture') {
-    showPictures();
+  if (id == '/pictureOne') {
+    showPicturesOne();
   }
-  console.log(event);
+  if (id == '/pictureTwo') {
+    showPicturesTwo();
+  }
+  if (id == '/skip') {
+    showPicturesSkip();
+  }
 });
