68976a15bef07ee4f3e5200befe68a949f6d4961
Hyun <lhc4128@gmail.com>
2016-03-30 11:18:31 -0700

    I added a button on pictures and used event delegation to register clicks. Skip button works fine but clicks on pictures do not recognize the event. #3

--- public/default.js
+++ public/default.js
@@ -1,8 +1,7 @@
 var imagePanel = document.getElementById('image-panel');
+imagePanel.setAttribute('id', 'images');
 
-var pictureButton = document.getElementById('show-pictures');
-
-pictureButton.addEventListener('click', function() {
+function showPictures() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', '/picture');
   xhr.send();
@@ -21,19 +20,46 @@ pictureButton.addEventListener('click', function() {
       pictureTwo.setAttribute('src', result[1]);
       pictureTwo.setAttribute('class', 'img-responsive')
 
+      var buttonOne = document.createElement('button');
+      buttonOne.setAttribute('directory', '/pictures');
+      buttonOne.setAttribute('type', 'button');
+
+      var buttonTwo = document.createElement('button');
+      buttonTwo.setAttribute('directory', '/pictures')
+      buttonTwo.setAttribute('type', 'button');
+
       var firstPanel = document.createElement('div');
-      firstPanel.setAttribute('class', 'col-md-6');
+      firstPanel.setAttribute('class', 'col-md-5');
 
       var secondPanel = document.createElement('div');
-      secondPanel.setAttribute('class', 'col-md-6');
+      secondPanel.setAttribute('class', 'col-md-5 col-md-offset-2');
 
-      firstPanel.appendChild(pictureOne);
-      secondPanel.appendChild(pictureTwo);
+      var or = document.createElement('div');
+      or.setAttribute('class', 'col-md-2 text-center');
+      or.setAttribute('id', 'or')
+      or.textContent = 'OR';
+
+      buttonOne.appendChild(pictureOne);
+      buttonTwo.appendChild(pictureTwo);
+
+      firstPanel.appendChild(buttonOne);
+      secondPanel.appendChild(buttonTwo);
 
       imagePanel.appendChild(firstPanel);
+      imagePanel.appendChild(or);
       imagePanel.appendChild(secondPanel);
     } else {
       alert('there was an error');
     }
   })
-})
+}
+
+var body = document.body;
+body.addEventListener('click', function(event) {
+  event.preventDefault();
+  var id = event.target.getAttribute('directory');
+  if (id == '/picture') {
+    console.log(event);
+    showPictures();
+  }
+});
